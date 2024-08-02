fba4db18b8628379b38074d9a08dbd7013d36a54
Alexa Peduzzi <alexapeduzzi@gmail.com>
2024-08-02 10:49:39 -0400

    Updated copy

--- playground-plugin-admin-notice.php
+++ playground-plugin-admin-notice.php
@@ -17,7 +17,7 @@
     ?>
     <div class="notice notice-info">
         <p><?php _e($custom_message, 'playground-plugin-admin-notice'); ?></p>
-        <p> This is a test to try the GitHub Deployments </p>
+        <p> This is a test to try the GitHub Deployments (Alexa's version) </p>
     </div>
     <?php
 });
